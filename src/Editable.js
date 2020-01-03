1cceba3b4adf5f3ab0d20b12e24b168d44cb6020
Yassien <11986226+YassienW@users.noreply.github.com>
2020-01-03 19:04:08 +0200

    Replaced FontAwesome packages with svgs, resulting in a major size reduction

--- src/Editable.js
+++ src/Editable.js
@@ -6,8 +6,14 @@ import Select from "./components/Select";
 import Date from "./components/Date";
 import {Button, Form, PopoverHeader, PopoverBody, Popover, Spinner, FormText} from "reactstrap";
 import File from "./components/File";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCheck, faTimes} from '@fortawesome/free-solid-svg-icons';
+
+const fontAwesomeStyle = {
+    textAlign: "center",
+    width: "1.25em",
+    height: "1em",
+    fill: "white",
+    verticalAlign: "-0.25em"
+};
 
 export default class Editable extends React.Component{
     constructor(props){
@@ -39,10 +45,14 @@ export default class Editable extends React.Component{
         let controls = (
             <React.Fragment>
                 <Button className="ml-auto mr-1" color="success" size="sm" onClick={() => this.onSubmit(this.state.newValue)}>
-                    <FontAwesomeIcon icon={faCheck} fixedWidth/>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={fontAwesomeStyle}>
+                        <path color="white" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/>
+                    </svg>
                 </Button>
                 <Button color="danger" size="sm" onClick={() => this.onCancel()}>
-                    <FontAwesomeIcon icon={faTimes} fixedWidth/>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" style={fontAwesomeStyle}>
+                        <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/>
+                    </svg>
                 </Button>
             </React.Fragment>
         );
