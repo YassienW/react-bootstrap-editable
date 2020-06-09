9e2fb854e44c683bddd4bb53c249b860f9af557f
Yassien <11986226+YassienW@users.noreply.github.com>
2020-06-09 21:21:30 +0200

    Remove onClick handler in favour of onFormSubmit function

--- src/Editable.js
+++ src/Editable.js
@@ -43,7 +43,7 @@ export default class Editable extends React.Component{
     }
     getEditingComponent(){
         let confirmButton = (
-            <Button className="ml-auto mr-1" color="success" size="sm" onClick={() => this.onSubmit(this.state.newValue)}>
+            <Button className="ml-auto mr-1" color="success" size="sm">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={fontAwesomeStyle}>
                     <path color="white" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/>
                 </svg>
