255b86ba1986324c3f481554a1c9058fb9c1216b
Yassien <11986226+YassienW@users.noreply.github.com>
2020-07-13 17:03:57 +0200

    Handle values that can't be parsed to date objects

--- src/components/Date.js
+++ src/components/Date.js
@@ -3,9 +3,19 @@ import {Input} from "reactstrap";
 
 export default function Date({value, setNewValue, controls}){
     let date = value
+
     if(date){
-        date = date.toISOString? date.toISOString().slice(0, 10): new window.Date(date).toISOString().slice(0, 10)
+        if(date.toISOString){
+            date = date.toISOString().slice(0, 10)
+        }else{
+            try{
+                date = new window.Date(date).toISOString().slice(0, 10)
+            }catch(err){
+                console.error(`Unable to parse provided date value to Date object: ${err.message}`)
+            }
+        }
     }
+
     return (
         <React.Fragment>
             <Input value={date? date : ""} type="date" bsSize="sm" className="mr-1"
